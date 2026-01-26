531daf32ba35a39b14457f3ad4e925f8cf8f1865
Tsim Muaj Yang <tyang4204@gmail.com>
2026-01-26 11:55:45 -0600

    got environment working

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss],
+  plugins: [react(), tailwindcss()],
 });
